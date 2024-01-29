645e79669b9f32af51a93207a5d05de4efa8f8ef
Luis Velasquez <luisfer2432@gmail.com>
2024-01-29 08:38:54 -0600

    Primeros archivos

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css"
+import { Navbar } from "./Navbar";
+import { Footer } from "./Footer";
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <h1 className="title">Este es el titulo</h1>
+      <h2>Un subtitulo</h2>
+      <h2>Algo mas</h2>
+      < Footer />
+    </div>
+  )
+}
+
+export default App
